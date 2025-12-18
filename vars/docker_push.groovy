fe1320216dea44559bdb34dc42018f708e333e51
Ashutosh Samantaray <116368054+Shiba07s@users.noreply.github.com>
2025-12-19 01:59:25 +0530

    Create docker_push.groovy

---
+++ vars/docker_push.groovy
@@ -0,0 +1,10 @@
+def call(String projectName,String imageTag,String dockerHubUser){
+  withCredentials([usernamePassword(credentialsId: "dockerHubCred",passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")]){
+    sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+  }
+  // sh "docker image tag product-service:latest ${env.dockerHubUser}/product-service:latest"
+  sh "docker push ${dockerHubUser}/&{projectName}:${imageTag}"
+  echo "docker push completed" 
+                 
+
+}
